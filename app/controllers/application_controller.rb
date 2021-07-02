20317ab995f1b0b5aa5cbb875e71ae9ee8c81e85
Daniel Collier <danny.collier@gmail.com>
2021-07-01 20:34:44 -0400

    Sign-in works. Alerts and sign-out do not.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,21 @@ class ApplicationController < ActionController::Base
 
   helper_method :current_user
 
-  def current_user
-    return @current_user if defined?(@current_user)
-    @current_user = true # current_user_session && current_user_session.user
+  protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  # def current_user
+  #   return @current_user if defined?(@current_user)
+  #   @current_user = true # current_user_session && current_user_session.user
+  # end
+
+  protected
+
+  def after_sign_in_path_for(users)
+    root_path
+  end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_in) { |u| u.permit(:username, :password, :remember_me) }
   end
 end
