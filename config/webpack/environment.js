05a55f3a4b8059adf4cf76a3a6e615658c91c69f
Daniel Collier <danny.collier@gmail.com>
2021-08-25 22:08:38 -0400

    Attempt to fix heroku asset compilation problem.

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,12 +1,22 @@
 const { environment } = require('@rails/webpacker')
 const webpack = require('webpack')
-
-environment.plugins.prepend('Provide',
+// Adds `var jQuery = require('jquery') to legacy jQuery plugins
+environment.plugins.append(
+    'Provide',
     new webpack.ProvidePlugin({
-        $: 'jquery/src/jquery',
-        jQuery: 'jquery/src/jquery',
+        $: 'jquery',
+        jQuery: 'jquery',
+        jquery: 'jquery',
         Popper: ['popper.js', 'default']
     })
 )
+
+// environment.plugins.prepend('Provide',
+//     new webpack.ProvidePlugin({
+//         $: 'jquery/src/jquery',
+//         jQuery: 'jquery/src/jquery',
+//         Popper: ['popper.js', 'default']
+//     })
+// )
 
 module.exports = environment
