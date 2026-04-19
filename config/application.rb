705449de2dcbee49920b03f9a6dd0002e73bffa8
Danny Collier <294724+dcollie2@users.noreply.github.com>
2026-04-18 20:37:51 -0400

    Hotfixes

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,8 @@ module Familydictionary4
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    # Disable image variant processing (image_processing gem not installed)
+    config.active_storage.variant_processor = :disabled
   end
 end
